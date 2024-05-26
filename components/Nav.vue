d8c5349e45dc1e67a5302b7db72b2516e65e3ac3
hazem hamdy <hh4morsy@gmail.com>
2024-05-26 20:16:59 +0300

    edit in nav

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     dir="rtl"
-    class="flex flex-col md:flex-row items-center md:justify-between  text-lg md:text-xl duration-300 transition-all bg-white/75 dark:bg-black/75 rounded-full px-2">
+    class="flex flex-col md:flex-row items-center md:justify-between text-lg md:text-xl duration-300 transition-all bg-white/75 dark:bg-black/75 rounded-full px-2">
     <div class="flex items-center justify-center gap-5 grow">
       <ULink to="#container" active-class="" inactive-class="text-gray-500">
         <img src="/logo (2).svg" class="h-16 md:h-20" />
@@ -9,7 +9,8 @@
 
       <h2>المميزة للسياحة</h2>
     </div>
-    <div class=" gap-5 justify-center grow xl:grow-0 hidden md:flex md:order-first">
+    <div
+      class="gap-5 justify-center grow xl:grow-0 hidden md:flex md:order-first">
       <ULink
         active-class=""
         inactive-class="text-gray-500"
@@ -26,17 +27,17 @@
         >خدماتنا</ULink
       >
     </div>
-    <div class="hidden gap-3 justify-center grow xl:grow-0 text-3xl  md:flex">
+    <div class="hidden gap-3 justify-center grow xl:grow-0 text-3xl md:flex">
       <div
-        class="group w-[45px] h-[45px] rounded-full bg-black/55 flex justify-center items-center hover:bg-amber-500 duration-300">
+        class="group w-[45px] h-[45px] rounded-full bg-white/55 dark:bg-black/55 flex justify-center items-center hover:bg-amber-500 duration-300">
         <UIcon name="i-mdi-facebook group-hover:text-black " />
       </div>
       <div
-        class="group w-[45px] h-[45px] rounded-full bg-black/55 flex justify-center items-center hover:bg-amber-500 duration-300">
+        class="group w-[45px] h-[45px] rounded-full bg-white/55 dark:bg-black/55 flex justify-center items-center hover:bg-amber-500 duration-300">
         <UIcon name="i-mdi-whatsapp group-hover:text-black " />
       </div>
       <div
-        class="group w-[45px] h-[45px] rounded-full bg-black/55 flex justify-center items-center hover:bg-amber-500 duration-300">
+        class="group w-[45px] h-[45px] rounded-full bg-white/55 dartk:bg-black/55 flex justify-center items-center hover:bg-amber-500 duration-300">
         <UIcon name="i-mdi-snapchat group-hover:text-black " />
       </div>
     </div>
