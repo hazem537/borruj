24d9b62d5df29e8b0a2f3f3558391861f5ce7b3b
hazem hamdy <74113376+hazem537@users.noreply.github.com>
2025-02-18 14:17:03 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
     class="text-stone-900 dark:text-stone-200 font-semibold flex flex-col gap-3   snap-mandatory "
     id="container">
     <Hero class="w-full h-screen snap-end" id="hero" />
-    <AboutUs  class="w-full h-screen snap-start" id="aboutUs"/>
+ 
     <Servise class="w-full h-screen snap-start " id="servise" />
     <ContactUs  class="w-full h-screen snap-start" id="contactUs"/>
     <Footer class="w-full snap-start " id="footer" />
@@ -13,7 +13,7 @@
 
 <script lang="ts" setup>
 // import Hero from '~/components/hero.vue';
-
+//   <AboutUs  class="w-full h-screen snap-start" id="aboutUs"/>
 </script>
 
 <style></style>
