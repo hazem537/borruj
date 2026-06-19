18fa0614572c34ac80b60054e9f8361930c90ee4
hazem hamdy <74113376+hazem537@users.noreply.github.com>
2026-06-19 14:09:06 +0300

    updat contact us

--- components/ContactUs.vue
+++ components/ContactUs.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="flex flex-col justify-center items-center text-sm md:text-base p-4">
+  <div
+    class="flex flex-col justify-center items-center text-sm md:text-base p-4"
+  >
     <div
-      class="flex flex-col gap-3 bg-white/55 dark:bg-black rounded-xl border items-center border-amber-500 p-5">
+      class="flex flex-col gap-3 bg-white/55 dark:bg-black rounded-xl border items-center border-amber-500 p-5"
+    >
       <h2 class="text-2xl text-amber-500 text-center">اتصل بنا</h2>
       <p>
         نسعد بتواصلكم معنا لأي استفسارات أو حجوزات. يرجى ملء النموذج أدناه
@@ -12,8 +15,9 @@
           class="flex flex-col gap-2 w-full"
           :schema="schema"
           :state="state"
-          @submit="onSubmit">
-          <div class="flex flex-col md:flex-row  w-full gap-3 justify-between">
+          @submit="onSubmit"
+        >
+          <div class="flex flex-col md:flex-row w-full gap-3 justify-between">
             <UFormGroup label="الاسم" name="name" class="grow">
               <UInput v-model="state.name" />
             </UFormGroup>
@@ -30,29 +34,28 @@
             <UTextarea v-model="state.message" />
           </UFormGroup>
 
-          <UButton block type="submit" color="amber" :loading="pending"> ارسل لنا </UButton>
+          <UButton block type="submit" color="amber" :loading="pending">
+            ارسل لنا
+          </UButton>
         </UForm>
       </div>
       <div class="flex flex-col md:flex-row">
         <div class="flex flex-col p-2 gap-2">
           <h2 class="text-2xl text-amber-500 text-center">تفاصيل الاتصال</h2>
           <p class="">
-            <span class="text-amber-500">
-              الهاتف
-            </span>
+            <span class="text-amber-500"> الهاتف </span>
             : 0507583933
             <br />
-            <span class="text-amber-500">
-              البريد الإلكتروني
-            </span>
+            <span class="text-amber-500"> البريد الإلكتروني </span>
             :Buroujtourism@gmail.com
           </p>
         </div>
         <div class="flex flex-col p-2 gap-2">
           <h2 class="text-2xl text-amber-500 text-center">ساعات العمل</h2>
           <p class="">
-          العمل طوال أيام الأسبوع
-خلال 24 ساعة للرد على الطلبات والاستفسارات والشكاو  
+            العمل طوال أيام الأسبوع
+            <br />
+            خلال 24 ساعة للرد على الطلبات والاستفسارات والشكاوي
           </p>
         </div>
       </div>
@@ -62,7 +65,7 @@
 
 <script lang="ts" setup>
 import { object, string } from "yup";
-const pending = ref(false)
+const pending = ref(false);
 const schema = object({
   name: string().required(),
   email: string().email().required(),
@@ -70,21 +73,20 @@ const schema = object({
   message: string().required(),
 });
 const state = reactive({ name: "", email: "", object: "", message: "" });
-import {  setDoc,addDoc,doc, collection } from "firebase/firestore";
+import { setDoc, addDoc, doc, collection } from "firebase/firestore";
 const db = useFirestore();
 // const messageCollection = collection(db,"messages")
-const toast=useToast();
-const onSubmit = async() => {
-  try{
-   pending.value=true 
-    const docref=await addDoc(collection(db,'messages'),state)
+const toast = useToast();
+const onSubmit = async () => {
+  try {
+    pending.value = true;
+    const docref = await addDoc(collection(db, "messages"), state);
     // console.log("HELLO",docref.id)
-  }catch(error){
-console.log(error)
-  }finally{
-    pending.value=false
+  } catch (error) {
+    console.log(error);
+  } finally {
+    pending.value = false;
   }
-
 };
 </script>
 
